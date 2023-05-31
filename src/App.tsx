eb7ead51102fa4e07a58aab08d38f313a8d7830d
Rami Sabbagh <rami.sab07@gmail.com>
2023-05-31 17:44:59 +0300

    Add zoom in-out support

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ type CanvasLayerProps = {
 function CanvasLayer({ sourceImage, resultImage }: CanvasLayerProps) {
     const containerRef = useRef<HTMLDivElement>(null);
     const paneRef = useRef<HTMLDivElement>(null);
+    const overlayRef = useRef<HTMLDivElement>(null);
     const [dimensions, setDimensions] = useState<{ w: number, h: number } | null>(null);
 
     const centerCanvas = useCallback(() => {
@@ -39,56 +40,75 @@ function CanvasLayer({ sourceImage, resultImage }: CanvasLayerProps) {
     }, []);
 
     useEffect(() => {
-        const pane = paneRef.current, container = containerRef.current;
-        if (!pane || !container) return;
+        const container = containerRef.current, pane = paneRef.current, overlay = overlayRef.current;
+        if (!container || !pane || !overlay) return;
 
         let activePointer: number | null = null;
 
-        const listener = (ev: PointerEvent) => {
+        const pointerListener = (ev: PointerEvent) => {
             const { type, pointerId, movementX, movementY } = ev;
 
             if (type === 'pointerdown' && activePointer === null) activePointer = pointerId;
             else if ((type === 'pointerup' || type === 'pointerout') && activePointer === pointerId) activePointer = null;
 
-            pane.style.cursor = activePointer === null ? 'grab' : 'grabbing';
+            overlay.style.cursor = activePointer === null ? 'grab' : 'grabbing';
             if (activePointer !== pointerId) return;
 
             container.scrollBy(-movementX, -movementY);
             ev.preventDefault();
         };
 
-        pane.addEventListener('pointerdown', listener);
-        pane.addEventListener('pointermove', listener);
-        pane.addEventListener('pointerup', listener);
-        pane.addEventListener('pointerout', listener);
+        const wheelListener = (ev: WheelEvent) => {
+            const scaleRaw = pane.style.scale;
+            const scale = Number.parseFloat(scaleRaw === '' ? '1' : scaleRaw);
+            const delta = ev.deltaY * -1e-3 * Math.ceil(scale);
+
+            if (scale + delta < 0.1) return;
+            if (scale + delta > 20) return;
+
+            pane.style.scale = (scale + delta).toString();
+        };
+
+        overlay.addEventListener('pointerdown', pointerListener);
+        overlay.addEventListener('pointermove', pointerListener);
+        overlay.addEventListener('pointerup', pointerListener);
+        overlay.addEventListener('pointerout', pointerListener);
+        overlay.addEventListener('wheel', wheelListener);
 
         return () => {
-            pane.removeEventListener('pointerdown', listener);
-            pane.removeEventListener('pointermove', listener);
-            pane.removeEventListener('pointerup', listener);
-            pane.removeEventListener('pointerout', listener);
+            overlay.removeEventListener('pointerdown', pointerListener);
+            overlay.removeEventListener('pointermove', pointerListener);
+            overlay.removeEventListener('pointerup', pointerListener);
+            overlay.removeEventListener('pointerout', pointerListener);
+            overlay.removeEventListener('wheel', wheelListener);
         };
     }, [centerCanvas]);
 
     const onLoad = useCallback<React.ReactEventHandler<HTMLImageElement>>((ev) => {
+        const pane = paneRef.current;
+        if (pane) pane.style.scale = '1';
+
         setTimeout(centerCanvas, 20);
         setDimensions({ w: ev.currentTarget.width, h: ev.currentTarget.height });
     }, [centerCanvas]);
 
-    return <div ref={containerRef} className="canvas-layer">
-        <div ref={paneRef} className="canvas-pane" style={{
-            writingMode: (dimensions && dimensions.w <= dimensions.h) ? 'vertical-lr' : 'horizontal-tb'
-        }}>
-            <img src={sourceImage} alt='Original' onLoad={onLoad} />
-            {resultImage
-                ? <img src={resultImage} alt='Quantized' />
-                : <Skeleton
-                    variant='rectangular'
-                    width={dimensions?.w}
-                    height={dimensions?.h}
-                />}
+    return <>
+        <div ref={containerRef} className="canvas-layer">
+            <div ref={paneRef} className="canvas-pane" style={{
+                writingMode: (dimensions && dimensions.w <= dimensions.h) ? 'vertical-lr' : 'horizontal-tb'
+            }}>
+                <img src={sourceImage} alt='Original' onLoad={onLoad} />
+                {resultImage
+                    ? <img src={resultImage} alt='Quantized' />
+                    : <Skeleton
+                        variant='rectangular'
+                        width={dimensions?.w}
+                        height={dimensions?.h}
+                    />}
+            </div>
         </div>
-    </div>;
+        <div ref={overlayRef} className="canvas-input-overlay" />
+    </>;
 }
 
 type ToolBarProps = {
