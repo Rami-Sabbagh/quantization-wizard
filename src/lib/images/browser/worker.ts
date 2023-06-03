465ea3d8b077fef1b096c2191d44a01450d1f935
Hrayr Derbedrossian <94538722+hrayrdb@users.noreply.github.com>
2023-06-03 10:07:51 +0300

    median cut

--- src/lib/images/browser/worker.ts
+++ src/lib/images/browser/worker.ts
@@ -1,25 +1,44 @@
-import { fromImageData } from './imagedata';
-import { QuantizationResult, QuantizationTask } from './messages';
+import { fromImageData } from "./imagedata";
+import { QuantizationResult, QuantizationTask } from "./messages";
 
-import { kMeansSync } from '../quantization/k-means';
+import { kMeansSync } from "../quantization/k-means";
+import { medianCutSync } from "../quantization/median-cut";
 
-console.log('Worker started!');
+console.log("Worker started!");
 
 onmessage = ({ data: message }: MessageEvent<QuantizationTask>) => {
-    const { id, algorithm, data } = message;
+  const { id, algorithm, data } = message;
 
-    if (algorithm === 'k-means') {
-        console.log(`[Task ${id}]: Started processing an image of dimensions ${data.width}x${data.height}.`);
+  if (algorithm === "k-means") {
+    console.log(
+      `[Task ${id}]: Started processing an image of dimensions ${data.width}x${data.height}.`
+    );
 
-        const image = fromImageData(data);
-        const { palette, histogram } = kMeansSync(image, message.count);
+    const image = fromImageData(data);
+    const { palette, histogram } = kMeansSync(image, message.count);
 
-        postMessage({
-            id, data: image.toImageData(),
-            palette, histogram,
-        } satisfies QuantizationResult);
+    postMessage({
+      id,
+      data: image.toImageData(),
+      palette,
+      histogram,
+    } satisfies QuantizationResult);
+  } else {
+    if (algorithm === "median-cut") {
+      console.log(
+        `[Task ${id}]: Started processing an image of dimensions ${data.width}x${data.height}.`
+      );
+      const image = fromImageData(data);
+      const { palette, histogram } = medianCutSync(image, message.count);
 
+      postMessage({
+        id,
+        data: image.toImageData(),
+        palette,
+        histogram,
+      } satisfies QuantizationResult);
     } else {
-        throw new Error(`Unsupported algorithm '${algorithm}'.`);
+      throw new Error(`Unsupported algorithm '${algorithm}'.`);
     }
-}
+  }
+};
