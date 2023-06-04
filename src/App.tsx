b2fc37b495ab9f23e86e96bbae10244b4a7baef9
Rami Sabbagh <rami.sab07@gmail.com>
2023-06-04 05:32:27 +0300

    Split the toolbar component into it's own file

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,15 @@ import '@fontsource/roboto/700.css';
 import './App.scss';
 import defaultImage from './assets/gimp-2.10-splash.png';
 
-import { Button, IconButton, InputAdornment, MenuItem, OutlinedInput, Select, Skeleton, Tooltip } from '@mui/material';
-import FolderOpenIcon from '@mui/icons-material/FolderOpen';
-import SaveAltIcon from '@mui/icons-material/SaveAlt';
-import RefreshIcon from '@mui/icons-material/Refresh';
+import { Skeleton } from '@mui/material';
 
 import { saveAs } from 'file-saver';
 
 import { loadImageData, toDataURL } from './lib/images/browser/loader';
 import { QuantizationAlgorithm, quantize } from './lib/images/browser/async';
-import { NumericFormatCustom } from './components/numeric-format-custom';
-import { SelectChangeEvent } from '@mui/material/Select/SelectInput';
 import { RGBA } from './lib/images/interfaces';
+import { ToolBar } from './components/toolbar';
 
-const algorithmDisplayName: Record<QuantizationAlgorithm, string> = {
-    'k-means': 'Naïve k-Means',
-    'median-cut': 'Median Cut',
-    'octree': 'Octree',
-    'popularity': 'Popularity',
-};
 
 type CanvasLayerProps = {
     sourceImage: string;
@@ -123,123 +113,6 @@ function CanvasLayer({ sourceImage, resultImage }: CanvasLayerProps) {
         </div>
         <div ref={overlayRef} className="canvas-input-overlay" />
     </>;
-}
-
-type ToolBarProps = {
-    onLoadImage?: (imageFile: File) => void;
-    onSaveImage?: () => void;
-
-    algorithm?: QuantizationAlgorithm,
-    setAlgorithm?: (algorithm: QuantizationAlgorithm) => void,
-
-    paletteSize?: string,
-    setPaletteSize?: (size: string) => void,
-
-    reperformQuantization?: () => void;
-};
-
-function ToolBar({ onLoadImage, onSaveImage, algorithm, setAlgorithm, paletteSize, setPaletteSize, reperformQuantization }: ToolBarProps) {
-    const fileInputRef = useRef<HTMLInputElement>(null);
-
-    const openFileDialog = useCallback(() => {
-        fileInputRef.current?.click();
-    }, []);
-
-    type InputEventHandler = React.ChangeEventHandler<HTMLInputElement>;
-
-    const onFileInputChange = useCallback<InputEventHandler>((event) => {
-        if (!event.target.files || event.target.files.length === 0 || !onLoadImage) return;
-        onLoadImage(event.target.files[0]);
-    }, [onLoadImage]);
-
-    type AlgorithmChangeHandler = (event: SelectChangeEvent<QuantizationAlgorithm>) => void;
-
-    const onAlgorithmChange = useCallback<AlgorithmChangeHandler>((ev) => {
-        if (!setAlgorithm) return;
-        setAlgorithm(ev.target.value as QuantizationAlgorithm);
-    }, [setAlgorithm]);
-
-    type PaletteSizeChangeHandler = React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
-
-    const onPaletteSizeChange = useCallback<PaletteSizeChangeHandler>((ev) => {
-        if (!setPaletteSize) return;
-        setPaletteSize(ev.target.value);
-    }, [setPaletteSize]);
-
-    const paletteSizeParsed = Number.parseInt(paletteSize ?? '');
-    const paletteSizeInvalid = isNaN(paletteSizeParsed) || paletteSizeParsed < 1 || paletteSizeParsed > 256;
-
-    return <div className="toolbar">
-        <Button>
-            Quantization Wizard
-        </Button>
-
-        <Tooltip title="Open/Load Image">
-            <span>
-                <IconButton onClick={openFileDialog} disabled={!onLoadImage}>
-                    <FolderOpenIcon />
-                </IconButton>
-            </span>
-        </Tooltip>
-
-        <Tooltip title="Export/Save Image">
-            <span>
-                <IconButton onClick={onSaveImage} disabled={!onSaveImage}>
-                    <SaveAltIcon />
-                </IconButton>
-            </span>
-        </Tooltip>
-
-        <div className='spacer' />
-
-        {
-            algorithm !== undefined && <Select
-                id="algorithm"
-                value={algorithm}
-                onChange={onAlgorithmChange}
-                sx={{ width: '18ch' }}
-                margin='none'
-                disabled={setAlgorithm === undefined}
-            >
-                {Object.entries(algorithmDisplayName).map(([id, name]) =>
-                    <MenuItem key={id} value={id}>{name}</MenuItem>
-                )}
-            </Select>
-        }
-
-        {
-            paletteSize !== undefined && <OutlinedInput
-                id="palette-size"
-                value={paletteSize}
-                onChange={onPaletteSizeChange}
-                endAdornment={<InputAdornment position="end">colors</InputAdornment>}
-                size="small"
-                sx={{ width: '12ch' }}
-                margin='none'
-                inputProps={{
-                    inputComponent: NumericFormatCustom as any,
-                }}
-                error={paletteSizeInvalid}
-                disabled={setPaletteSize === undefined}
-            />
-        }
-
-        <Tooltip title="Reperform Quantization">
-            <span>
-                <IconButton onClick={reperformQuantization} disabled={!reperformQuantization}>
-                    <RefreshIcon />
-                </IconButton>
-            </span>
-        </Tooltip>
-
-        <input
-            type='file'
-            ref={fileInputRef}
-            onChange={onFileInputChange}
-            style={{ display: 'none' }}
-            accept='image/jpeg, image/png, image/bmp'
-        />
-    </div>;
 }
 
 function App() {
