c051db50dccb224f8eed50c01f916d0d0c92f94b
Haidar_Al_Sous <haidar.alsous.de@gmail.com>
2023-06-11 13:52:21 +0300

    D

--- src/lib/images/browser/Searchimages.ts
+++
@@ -1,9 +0,0 @@
-import { fromImageData } from "./imagedata";
-import { QuantizationResult, QuantizationTask } from "./messages";
-
-import { kMeansSync } from "../quantization/k-means";
-import { medianCutSync } from "../quantization/median-cut";
-import { octreeSync } from "../quantization/octree";
-import { popularitySync } from "../quantization/popularity";
-
-console.log("Searchimages started!");
