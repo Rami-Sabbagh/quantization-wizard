3b23fd00a7f9c857f04448c6c47995fa9fb2f7f6
Rami Sabbagh <rami.sab07@gmail.com>
2023-06-18 23:16:09 +0300

    Add a bunch of buttons to the similar search toolbar

--- src/routes/similar-search/toolbar.tsx
+++ src/routes/similar-search/toolbar.tsx
@@ -1,6 +1,34 @@
 import React from 'react';
 
+import { IconButton, Tooltip } from '@mui/material';
+import FolderOpenIcon from '@mui/icons-material/FolderOpen';
+import ClearIcon from '@mui/icons-material/Clear';
+import ImageIcon from '@mui/icons-material/Image';
+import SaveAltIcon from '@mui/icons-material/SaveAlt';
+import PageviewIcon from '@mui/icons-material/Pageview';
+// import CropIcon from '@mui/icons-material/Crop';
+// import ImageAspectRatioIcon from '@mui/icons-material/ImageAspectRatio';
+import TuneIcon from '@mui/icons-material/Tune';
+import RefreshIcon from '@mui/icons-material/Refresh';
+
 import { AppMode, AppModeSwitch } from 'components/app-mode-switch';
+
+interface ToolBarIconButtonProp {
+    title: string;
+    icon: JSX.Element;
+
+    onClick?: () => void;
+}
+
+function ToolBarIconButton({ title, icon, onClick }: ToolBarIconButtonProp) {
+    return <Tooltip title={title}>
+        <span>
+            <IconButton disabled={!onClick} onClick={onClick}>
+                {icon}
+            </IconButton>
+        </span>
+    </Tooltip>;
+}
 
 interface ToolBarProps {
     setMode?: (mode: AppMode) => void;
@@ -10,7 +38,53 @@ export function ToolBar({ setMode }: ToolBarProps) {
     return <div className="toolbar">
         {setMode && <AppModeSwitch active="similar-search" setMode={setMode} />}
 
+        <ToolBarIconButton
+            title="Open/Load Source Images"
+            icon={<FolderOpenIcon />}
+            onClick={() => { }}
+        />
+
+        <ToolBarIconButton
+            title="Clear Source Images"
+            icon={<ClearIcon />}
+            onClick={() => { }}
+        />
+
+        <div className="separator" />
+
+        <ToolBarIconButton
+            title="Open/Load Target Image"
+            icon={<ImageIcon />}
+            onClick={() => { }}
+        />
+
+        <div className="separator" />
+
+        <ToolBarIconButton
+            title="Export/Save Similar Images"
+            icon={<SaveAltIcon />}
+            onClick={() => { }}
+        />
+
+        <ToolBarIconButton
+            title="Show Similar Images Path"
+            icon={<PageviewIcon />}
+            onClick={() => { }}
+        />
+
         <div className="spacer" />
+
+        <ToolBarIconButton
+            title="Search Options"
+            icon={<TuneIcon />}
+            onClick={() => { }}
+        />
+
+        <ToolBarIconButton
+            title="Research"
+            icon={<RefreshIcon />}
+            onClick={() => { }}
+        />
 
     </div>;
 }
