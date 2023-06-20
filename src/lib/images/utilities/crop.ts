8b49fa1587f8e35a1ca80c24a0fedd55d7cf0a5e
Rami Sabbagh <rami.sab07@gmail.com>
2023-06-20 13:42:43 +0300

    Reformat files

--- src/lib/images/utilities/crop.ts
+++ src/lib/images/utilities/crop.ts
@@ -8,24 +8,23 @@
      * @param maxY The Y coordinates of the bottom-right corner.
      */
 
-export function crop(image: ImageData, minX: number, minY: number, maxX: number, maxY: number): 
-ImageData {
-    
-// Calculate the dimensions of the cropped region
-const width = maxX - minX;
-const height = maxY - minY;
+export function crop(image: ImageData, minX: number, minY: number, maxX: number, maxY: number): ImageData {
 
-// Create a new ImageData object for the cropped region
-const croppedImage = new ImageData(width, height);
+    // Calculate the dimensions of the cropped region
+    const width = maxX - minX;
+    const height = maxY - minY;
 
-// Copy the pixels from the original image to the cropped image
-for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-        const srcIdx = ((y + minY) * image.width + (x + minX)) * 4;
-        const destIdx = (y * width + x) * 4;
-        croppedImage.data.set(image.data.slice(srcIdx, srcIdx + 4), destIdx);
+    // Create a new ImageData object for the cropped region
+    const croppedImage = new ImageData(width, height);
+
+    // Copy the pixels from the original image to the cropped image
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const srcIdx = ((y + minY) * image.width + (x + minX)) * 4;
+            const destIdx = (y * width + x) * 4;
+            croppedImage.data.set(image.data.slice(srcIdx, srcIdx + 4), destIdx);
+        }
     }
-}
 
-return croppedImage;
+    return croppedImage;
 }
--- src/lib/images/utilities/downscale.ts
+++ src/lib/images/utilities/downscale.ts
@@ -1,41 +1,38 @@
-    /**
-     * Resize an image **down**.
-     * 
-     * @param image The data of the image to downscale.
-     * @param width The target width. Must be <= the original width.
-     * @param height The target height. Must be <= the original height.
-     */
+/**
+ * Resize an image **down**.
+ * 
+ * @param image The data of the image to downscale.
+ * @param width The target width. Must be <= the original width.
+ * @param height The target height. Must be <= the original height.
+ */
+export function downscale(image: ImageData, width: number, height: number): ImageData {
+    if (width > image.width || height > image.height)
+        throw new Error('Target dimensions must be <= original dimensions.');
 
-
-    export function downscale(image: ImageData, width: number, height: number): 
-    ImageData {
-    if (width > image.width || height > image.height) {
-      throw new Error('Target dimensions must be <= original dimensions.');
-    }
-  
     // Create an in-memory canvas and context
     const canvas = document.createElement('canvas');
+
     canvas.width = image.width;
     canvas.height = image.height;
+
     const ctx = canvas.getContext('2d');
     if (!ctx) {
-      throw new Error('Could not create canvas context.');
+        throw new Error('Could not create canvas context.');
     }
-  
+
     // Draw the original image onto the canvas
     ctx.putImageData(image, 0, 0);
-  
+
     // Create a new canvas with the target dimensions and draw the original canvas onto it
     const scaledCanvas = document.createElement('canvas');
     scaledCanvas.width = width;
     scaledCanvas.height = height;
     const scaledCtx = scaledCanvas.getContext('2d');
-    if (!scaledCtx) {
-      throw new Error('Could not create scaled canvas context.');
-    }
+    if (!scaledCtx) throw new Error('Could not create scaled canvas context.');
+
     scaledCtx.imageSmoothingEnabled = false;
     scaledCtx.drawImage(canvas, 0, 0, image.width, image.height, 0, 0, width, height);
-  
+
     // Return the pixel data of the scaled image
     return scaledCtx.getImageData(0, 0, width, height);
-  }
+}
